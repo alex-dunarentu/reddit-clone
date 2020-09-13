1563bb7bf81238403be321ac677187f2772c4830
Alexandru-Nicolae DUNĂRENŢU (102451) <alexandru.dunarentu@stud.etti.upb.ro>
2020-09-13 23:32:40 +0200

    Updates

---
+++ precache-manifest.525d898c66a6a8125e8c2cc664632ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaeb1fb0e767e43e10f9e110d2064000",
+    "url": "/reddit-clone/index.html"
+  },
+  {
+    "revision": "cfd6587d8da1282895c5",
+    "url": "/reddit-clone/static/css/main.4c3c1b7a.chunk.css"
+  },
+  {
+    "revision": "ffda60a39c9ab1203f04",
+    "url": "/reddit-clone/static/js/2.3cbaf4d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-clone/static/js/2.3cbaf4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd6587d8da1282895c5",
+    "url": "/reddit-clone/static/js/main.5e978128.chunk.js"
+  },
+  {
+    "revision": "1573f77388e17e8a72fc",
+    "url": "/reddit-clone/static/js/runtime-main.0827c06d.js"
+  }
+]);
